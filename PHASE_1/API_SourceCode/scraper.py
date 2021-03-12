a584b8bb0f84219d8dda99246935fd22f19d325d
JA <41533743+z5015341@users.noreply.github.com>
2021-03-12 19:34:43 +1100

    change for scraping multiple pages and categories

--- PHASE_1/API_SourceCode/scraper.py
+++ PHASE_1/API_SourceCode/scraper.py
@@ -3,41 +3,47 @@ from bs4 import BeautifulSoup
 import json
 import os
 
-URL = 'http://outbreaknewstoday.com/category/research/'
-page = requests.get(URL)
-
-mainSoup = BeautifulSoup(page.content, 'html.parser')
-catArticles = mainSoup.find("div", {"class": "post_area"})
-articleSides = catArticles.find_all("div", {"class": "rightconside"})
-links = []
-for link in articleSides:
-    links.append(link.find("a")['href'])
+numberOfPages = 2
+categories = ['vaccines','research']
 
 f = open("article.json", "w")
 f.write("[\n")
 f.close()
 
-for l in links:
-    URL = l
-    page = requests.get(URL)
+for cat in categories:
+    for count in range(1,numberOfPages+1):
+        URL = 'http://outbreaknewstoday.com/category/' + cat + '/page/' + str(count)
+        page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    article = soup.find("div", {"class": "postsingle"})
-    title = article.find('h1')
-    para = article.find_all('p')
+        mainSoup = BeautifulSoup(page.content, 'html.parser')
+        catArticles = mainSoup.find("div", {"class": "post_area"})
+        articleSides = catArticles.find_all("div", {"class": "rightconside"})
+        links = []
+        for link in articleSides:
+            links.append(link.find("a")['href'])
 
-    json_data = {"Title":[],"Article":[]}
-    json_data["Title"].append(title.text)
 
-    for p in para:
-        json_data["Article"].append(p.text)
+        for l in links:
+            URL = l
+            page = requests.get(URL)
 
-    f = open("article.json", "a")
-    f.write(json.dumps(json_data,indent=4))
-    f.write(",\n")
-    f.close()
+            soup = BeautifulSoup(page.content, 'html.parser')
+            article = soup.find("div", {"class": "postsingle"})
+            title = article.find('h1')
+            para = article.find_all('p')
 
-remove_chars = len(os.linesep)
+            json_data = {"Title":[],"Article":[]}
+            json_data["Title"].append(title.text)
+
+            for p in para:
+                json_data["Article"].append(p.text)
+
+            f = open("article.json", "a")
+            f.write(json.dumps(json_data,indent=4))
+            f.write(",\n")
+            f.close()
+
+        remove_chars = len(os.linesep)
 
 f = open("article.json", "a")
 f.truncate(f.tell() - remove_chars * 2)
