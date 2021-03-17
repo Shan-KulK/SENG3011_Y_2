ce42fda6bd7a508a101c11cf571ba362151a9190
Shan-KulK <shantanu.kulkarni9@hotmail.com>
2021-03-18 03:34:48 +1100

    Pushing updated scraper + sample article + AWS Comprehend output

--- PHASE_1/API_SourceCode/scraper.py
+++ PHASE_1/API_SourceCode/scraper.py
@@ -32,12 +32,23 @@ for cat in categories:
             title = article.find('h1')
             para = article.find_all('p')
 
+            meta_data = article.find("div", {"class": "single_meta"})
+            date_object = meta_data.find("div", {"class": "datesingle"})
+            date_value = date_object.find("div", {"class":"datsingle"})
+            para = article.find_all('p')
+            for span_tag in date_value.findAll('span'):
+                span_tag.replace_with('')
+
+            print(date_value.text)
             json_data = {"Title":[],"Article":[]}
             json_data["Title"].append(title.text)
 
             for p in para:
                 json_data["Article"].append(p.text)
 
+            text = ''.join(json_data["Article"])
+            # print(text)
+            json_data["Article"] = text
             f = open("article.json", "a")
             f.write(json.dumps(json_data,indent=4))
             f.write(",\n")
