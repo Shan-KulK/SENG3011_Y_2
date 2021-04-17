01a793616b87bcf1101433de44e86887e03aa8f1
JA <41533743+z5015341@users.noreply.github.com>
2021-04-17 16:45:46 +1000

    Updated health scraper to include status and latest

---
+++ PHASE_2/Application_SourceCode/health_scraper.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+countries = ['egypt']#,'india','peru','china','russia','south-africa','brazil','new-zealand','italy','germany']
+
+f = open("health.json", "w")
+f.write("[\n")
+f.close()
+
+for place in countries:
+    URL = 'https://www.smartraveller.gov.au/destinations/' + place
+    page = requests.get(URL)
+
+    mainSoup = BeautifulSoup(page.content, 'html.parser')
+    healthSummary = mainSoup.find("div", {"class": "health paragraph paragraph--type--overview paragraph--view-mode--default"})
+    healthSummaryPoints = healthSummary.find("ul").text.encode('ascii', 'ignore').decode("utf-8")
+
+    overall = mainSoup.find("div", {"class": "views-field views-field-field-overall-advice-level"})
+    level = overall.find("div").text.encode('ascii', 'ignore').decode("utf-8").replace("Overall: ",'')
+
+    latest = mainSoup.find("div", {"id": "block-views-block-sidebars-last-updated"})
+    latestDiv = latest.find("div", {"class":"views-field views-field-field-last-update"})
+    latestUpdate = latestDiv.find("span", {"class":"right"}).text.encode('ascii', 'ignore').decode("utf-8")
+
+    content = ""
+    healthArticle = mainSoup.find_all("div",{"class": "clearfix text-formatted field field--name-field-body field--type-text-long field--label-hidden field__item"})
+    for h in healthArticle:
+        sib = h.find_previous_sibling('div')
+        try:
+            s = sib.find('h4').text
+            if(s == "Health risks"):
+                content = h.text.encode('ascii', 'ignore').decode("utf-8")
+                break
+        except:
+            pass
+
+    json_data = {"Country":[],"Status":[],"Latest":[],"Summary":[],"Info":[]}
+    json_data["Country"].append(place)
+    json_data["Status"].append(level)
+    json_data["Latest"].append(latestUpdate)
+    json_data["Summary"].append(healthSummaryPoints)
+    json_data["Info"].append(content)
+
+    f = open("health.json", "a")
+    f.write(json.dumps(json_data,indent=4))
+    if countries.index(place) == len(countries)-1:
+        f.write("\n")
+    else:
+        f.write(",\n")
+    f.close()
+
+f = open("health.json", "a")
+f.write("]")
+f.close()
